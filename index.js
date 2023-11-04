5ca90bdf1408002303233499c1d33311456365be
sakibsarkar <sakibsarkar707@gmail.com>
2023-11-04 19:13:02 +0600

    server created

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const port = process.env.PORT || 5000
+const app = express()
+
+
+
+// mid were
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("server is running ")
+})
+
+app.listen(port, () => {
+    console.log(`port is ${port}`)
+})
