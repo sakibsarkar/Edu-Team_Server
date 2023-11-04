6cc9c80297a09cd431760b3b6f1ff42c9da8d658
sakibsarkar <sakibsarkar707@gmail.com>
2023-11-04 19:34:43 +0600

    api created for adding a new assignment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const cors = require("cors")
 const port = process.env.PORT || 5000
@@ -8,6 +9,50 @@ const app = express()
 // mid were
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.xbiw867.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+
+        const assignmentCollection = client.db("AssignmentDB").collection("AssignmentCollection")
+
+
+        // adding a new assignment
+        app.post("/api/user/assignment/post", async (req, res) => {
+            const body = req.body
+            const result = await assignmentCollection.insertOne(body)
+            res.send(result)
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
